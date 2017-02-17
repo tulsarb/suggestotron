ae9af51825068fb99e34614d29be75e2881a047b
Wassim Metallaoui <wassim@metallaoui.com>
2017-02-16 18:57:50 -0600

    Switch back to built-in memcached

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,7 +48,7 @@ Rails.application.configure do
   config.log_tags = [:request_id]
 
   # Use a different cache store in production.
-  config.cache_store = :dalli_store, nil, { expires_in: 1.day, compress: true }
+  config.cache_store = :mem_cache_store, ENV['MEMCACHE_SERVERS']
 
   # Use a real queuing backend for Active Job (and separate queues per environment)
   # config.active_job.queue_adapter     = :resque
