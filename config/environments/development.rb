98833ca42c064b568eea08bcb13dc6b2538988f8
Wassim Metallaoui <wassim@metallaoui.com>
2017-02-18 07:32:58 -0600

    Add authentication layer

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -51,4 +51,7 @@ Rails.application.configure do
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   config.file_watcher = ActiveSupport::EventedFileUpdateChecker
+
+  # Default mail domain
+  config.action_mailer.default_url_options = { host: ENV['MAIL_HOST'], port: 3000 }
 end
