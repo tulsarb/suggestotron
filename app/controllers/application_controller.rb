b76cf2281cd551f63f43029b51ff6c3ad6edcecd
Wassim Metallaoui <wassim@metallaoui.com>
2017-02-18 09:15:52 -0600

    Add user content to Sentry.io reports

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,4 +2,13 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
   before_action :authenticate_user!
+  before_action :set_raven_context
+
+  private
+
+  def set_raven_context
+    Raven.user_context(id: current_user&.id,
+                       email: current_user&.email)
+    Raven.extra_context(params: params.to_unsafe_h)
+  end
 end
