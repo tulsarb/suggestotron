6d89e68deaa28577cf383dc4356dc5c1fcfef22c
Wassim Metallaoui <wassim@metallaoui.com>
2017-02-18 11:02:44 -0600

    Add sign in and out helper paths

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,15 @@
 Rails.application.routes.draw do
   devise_for :users
-  root 'topics#index'
+  devise_scope :user do
+    get 'sign_in', to: 'devise/sessions#new'
+    delete 'sign_out', to: 'devise/sessions#destroy'
+  end
+
   resources :topics do
     member do
       post 'upvote'
     end
   end
+
+  root 'topics#index'
 end
