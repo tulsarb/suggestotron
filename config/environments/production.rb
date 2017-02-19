400aa4b195b3d03b712449f69a9a1425ff85e615
Wassim Metallaoui <wassim@metallaoui.com>
2017-02-19 11:02:21 -0600

    Set default mailer queue

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -53,6 +53,7 @@ Rails.application.configure do
   # Use a real queuing backend for Active Job (and separate queues per environment)
   config.active_job.queue_adapter     = :active_elastic_job
   config.active_job.queue_name_prefix = "suggestotron_#{Rails.env}"
+  config.action_mailer.deliver_later_queue_name = :default
   config.action_mailer.perform_caching = false
 
   # Ignore bad email addresses and do not raise email delivery errors.
