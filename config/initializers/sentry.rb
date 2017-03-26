b142513b6b33e8f7d5725c15e1c23a45fee757a0
Wassim Metallaoui <wassim@metallaoui.com>
2017-03-26 13:46:49 -0500

    Sentry will use SENTRY_DSN ENV by default

--- config/initializers/sentry.rb
+++ config/initializers/sentry.rb
@@ -1,6 +1,4 @@
 Raven.configure do |config|
-  config.dsn = ENV['SENTRY_DSN']
   config.sanitize_fields = Rails.application.config.filter_parameters.map(&:to_s)
-  config.environments = %w(staging production)
   config.silence_ready = true
 end
