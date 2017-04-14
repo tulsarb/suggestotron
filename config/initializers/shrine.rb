304e5c5a3c367742dd392735c63387bd1255563b
Wassim Metallaoui <wassim@metallaoui.com>
2017-04-13 21:40:24 -0500

    Add a CDN URL prefix for CloudFront hosted images

--- config/initializers/shrine.rb
+++ config/initializers/shrine.rb
@@ -12,16 +12,19 @@ if Rails.env.production?
 
   Shrine.storages = {
     cache: Shrine::Storage::S3.new(prefix: 'cache', **s3_options),
-    store: Shrine::Storage::S3.new(prefix: 'store', **s3_options),
+    store: Shrine::Storage::S3.new(prefix: 'store',
+                                   upload_options: { acl: 'public-read' },
+                                   **s3_options)
   }
 else
   require 'shrine/storage/file_system'
 
   Shrine.storages = {
     cache: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/cache'),
-    store: Shrine::Storage::FileSystem.new("public", prefix: "uploads/store")
+    store: Shrine::Storage::FileSystem.new('public', prefix: 'uploads/store')
   }
 end
 
 Shrine.plugin :activerecord
 Shrine.plugin :cached_attachment_data
+Shrine.plugin :default_url_options, store: { public: true, host: ENV['CDN_URL'] }
