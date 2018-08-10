db6ba550b2c21edd9ba0d83ee52859f921a441ba
Wassim Metallaoui <wassim@metallaoui.com>
2018-08-10 10:37:25 -0500

    Add more tests to validate CircleCi parallel tests

--- spec/models/topic_spec.rb
+++ spec/models/topic_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 describe Topic do
+  describe 'Database' do
+    it { should have_db_column(:title).of_type(:string) }
+    it { should have_db_column(:description).of_type(:text) }
+  end
+
   describe 'Validations' do
     it { is_expected.to validate_presence_of(:title) }
   end
